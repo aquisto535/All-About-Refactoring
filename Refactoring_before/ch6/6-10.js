39d5425c189c1a22c5f8bec8476b90c4d3e33def
Jung Tae Min <71391013+aquisto535@users.noreply.github.com>
2025-08-24 23:23:51 +0900

    여러 함수를 변환 함수로 묶기

--- Refactoring_before/ch6/6-10.js
+++ Refactoring_before/ch6/6-10.js
@@ -1,7 +1,26 @@
+import _ from 'lodash'
+import { taxableCharge } from './6-9-client2';
+
 const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
 
 export function acquireReading() {
   return reading;
+}
+
+export function enrichReading(original)
+{
+  ///const result = {...original} //Object.assign: 얕은 복사
+
+  const result = _.cloneDeep(original);
+  result.baseCharge = caculateBaseCharge(result);
+  result.taxableCharge = Math.max(0, result.baseCharge - taxThereshold(result.year));
+
+  return result;
+}
+
+function caculateBaseCharge(reading)
+{
+  return baseRate(reading.month, reading.year) * reading.quantity;
 }
 
 export function baseRate(month, year) {
