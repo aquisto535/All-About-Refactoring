3a458f5bdb81aa7572cf61739334928544d50da4
Jung Tae Min <71391013+aquisto535@users.noreply.github.com>
2025-08-27 23:51:43 +0900

    함수 단계 쪼개기 챌린지1

--- Refactoring_before/ch6/6-11.js
+++ Refactoring_before/ch6/6-11.js
@@ -1,17 +1,107 @@
-export function priceOrder(product, quantity, shippingMethod) {
-  const basePrice = product.basePrice * quantity;
-  const discount =
-    Math.max(quantity - product.discountThreshold, 0) *
-    product.basePrice *
-    product.discountRate;
-  const shippingPerCase =
+export function priceOrder(product, quantity, shippingMethod) 
+{
+  //1. 기존 코드
+  //const basePrice = product.basePrice * quantity;
+
+  // const discount =
+  //   Math.max(quantity - product.discountThreshold, 0) *
+  //   product.basePrice *
+  //   product.discountRate;
+
+
+
+  // const shippingPerCase =
+  //   basePrice > shippingMethod.discountThreshold
+  //     ? shippingMethod.discountedFee
+  //     : shippingMethod.feePerCase;
+
+  //const shippingCost = quantity * shippingPerCase;
+
+  //--------------------------------------------------
+
+  //2. 내가 수정한 코드 
+
+  //기본 제품 가격
+  //const basePrice = MakeBasePrice(product, quantity);
+
+    //할인
+  //const discount = CalcDiscount(product, quantity);
+
+  //개별 배송비
+  //const shippingCost = CalcshippingCost(basePrice,quantity ,shippingMethod);
+
+  //const price = basePrice - discount + shippingCost;
+
+  //--------------------------------------------------
+
+  //3. 강의에서의 코드
+  
+  const basePrice = CalculateBasePrice(product, quantity);
+
+  const discount = CalculateDiscount(product, quantity);
+
+  const shippingCost = CalculateshippingCost(basePrice,quantity ,shippingMethod);
+
+  //const price = basePrice - discount + shippingCost;
+
+  return basePrice - discount + shippingCost;
+}
+
+//2. 내가 수정한 코드 
+// function MakeBasePrice(product, quantity)
+// {
+//   const basePrice = product.basePrice * quantity;
+
+//   return basePrice;
+
+// }
+
+// function CalcDiscount(product, quantity)
+// {
+//     const discount =
+//     Math.max(quantity - product.discountThreshold, 0) *
+//     product.basePrice *
+//     product.discountRate;
+
+//    return discount;
+// }
+
+// function CalcshippingCost(basePrice, quantity, shippingMethod)
+// {
+//     const shippingPerCase =
+//     basePrice > shippingMethod.discountThreshold
+//       ? shippingMethod.discountedFee
+//       : shippingMethod.feePerCase;
+
+//   const shippingCost = quantity * shippingPerCase;
+
+//   return shippingCost;
+// }
+
+//3. 강의에서의 코드
+function CalculateBasePrice(product, quantity)
+{
+  return product.basePrice * quantity;
+}
+
+function CalculateDiscount(product, quantity)
+{
+  return (Math.max(quantity - product.discountThreshold, 0) *
+     product.basePrice *
+     product.discountRate);
+}
+
+function CalculateshippingCost(basePrice, quantity, shippingMethod)
+{
+    const shippingPerCase =
     basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountedFee
       : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
-  return price;
+
+  return quantity * shippingPerCase;
 }
+
+
 
 // 사용 예:
 const product = {
